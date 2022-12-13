5accc1dec2bbbbda6ce292d5295f4e1c6adfcb88
h21anlus <h21anlus@du.se>
2022-12-13 12:56:34 +0100

    style changes

--- Components/BookDetails.js
+++ Components/BookDetails.js
@@ -1,5 +1,5 @@
 const BookDetailedInfo = (details) =>  {
-    let html = `<div class="book-list__details absolute border-neutral-800 border-2 bg-white right-20 rounded-[10px]" style="position: absolute; width: 10vw" id="bookDetail">
+    let html = `<div class="book-list__details absolute border-neutral-400 border-2 bg-white right-20 rounded-lg" style="position: absolute; width: 10vw" id="bookDetail">
                 <img src=${details.coverImage} alt="No image found" class="inline-block rounded-tl-lg rounded-tr-lg">
                 <h1 class="text-gray-900 text-xl font-medium mb-2">${details.title}</h4>
                 <p class="text-gray-700">Author: ${details.author}</p>
